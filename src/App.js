d7c07d43fc0255958528bbe5db88b787d468bc2a
sandeep jaiswar <jaiswarsandeep119@gmail.com>
2021-04-06 00:33:30 +0530

    restructured bolierplate code, configured firestore

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import './styles/index.css';
+import {getProductsFromFirestore,dataFromSnapshot} from './services/firestoreService'
 
 function App() {
+  useEffect(()=>{
+    const unsubscribe = getProductsFromFirestore({
+      next: snapshot => console.log(snapshot.docs.map(docSnapshot => dataFromSnapshot(docSnapshot))),
+      error: error => console.log(error),
+    });
+    return unsubscribe;
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
     </div>
-  );
+  )
 }
 
 export default App;
