97da536345fc81ccd08dd895d26c910408e87f6a
sandeep jaiswar <jaiswarsandeep119@gmail.com>
2021-04-07 17:54:05 +0530

    configured redux-thunk

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,6 +1,8 @@
-import {createStore} from 'redux';
+import {applyMiddleware, createStore} from 'redux';
 import rootReducer from "../reducers/rootReducer";
+import {composeWithDevTools} from "redux-devtools-extension";
+import thunk from 'redux-thunk';
 
 export function configureStore(){
-  return createStore(rootReducer);
+  return createStore(rootReducer,composeWithDevTools(applyMiddleware(thunk)));
 }
