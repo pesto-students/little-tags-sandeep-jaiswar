320351798df72482e7d0a71d7c73ce3804dfd106
sandeep jaiswar <jaiswarsandeep119@gmail.com>
2021-04-07 12:32:14 +0530

    added search bar to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,33 @@
 import React from 'react';
-import { FaUserAlt, FaSearch, FaShoppingCart,FaHeart } from 'react-icons/fa';
+import { FaUserAlt, FaSearch, FaShoppingCart, FaHeart } from 'react-icons/fa';
 import '../styles/components/Header.css';
 
 function Header(props) {
   return (
     <nav className="header">
-      <div className="hamburger" onClick={()=>props.state.setShowSidebar(!props.state.showSideBar)}>
-        <div className={props.state.showSideBar?"hamLine cross1":"hamLine"}></div>
-        <div className={props.state.showSideBar?"hamLine cross2":"hamLine"}></div>
-        <div className={props.state.showSideBar?"hamLine cross3":"hamLine"}></div>
+      <div className="hamburger" onClick={() => props.state.setShowSidebar(!props.state.showSideBar)}>
+        <div className={props.state.showSideBar ? "hamLine cross1" : "hamLine"}></div>
+        <div className={props.state.showSideBar ? "hamLine cross2" : "hamLine"}></div>
+        <div className={props.state.showSideBar ? "hamLine cross3" : "hamLine"}></div>
       </div>
-      <div className="logo">
+      {/* <div className="logo">
         <img src="https://icons.iconarchive.com/icons/social-media-icons/glossy-social/512/Spotify-icon.png" height="50px" width="50px" alt="logo" />
-      </div>
+      </div> */}
       <div className="container">
-        <div className="">
-          <FaSearch />
+        <div className="search-container">
+          <input type="text" name="search" placeholder="Search.." />
+          <button type="submit">
+            <FaSearch />
+          </button>
         </div>
         <div className="">
           <FaUserAlt />
         </div>
         <div className="">
-          <FaHeart/>
+          <FaHeart />
         </div>
         <div className="">
-          <FaShoppingCart/>
+          <FaShoppingCart />
         </div>
       </div>
     </nav>
