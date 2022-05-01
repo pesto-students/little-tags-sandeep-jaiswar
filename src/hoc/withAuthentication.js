ae7167963f90efd968ed0f5888e2c23874ac758b
sandeep-jaiswar <jaiswarsandeep119@gmail.com>
2022-05-01 14:09:52 +0530

    added changes

--- src/hoc/withAuthentication.js
+++ src/hoc/withAuthentication.js
@@ -2,55 +2,40 @@
 // import { connect } from 'react-redux';
 // import FirebaseContext from '../Config/Firebase/context';
 // import { setAuthUser } from '../actions';
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
-import { checkUserSession, userCheckedInSucess } from '../redux/User/user.actions';
-import Loader from '../UI/Loader/Loader';
+import { useDispatch, useSelector } from "react-redux"
+import { useEffect } from "react"
+import {
+  checkUserSession,
+  userCheckedInSucess,
+} from "../redux/User/user.actions"
+import Loader from "../UI/Loader/Loader"
 
 const mapState = (state) => ({
-    userCheck: state.user.userCheck,
-    userAddressFlag: state.user.userAddressFlag
-});
+  userCheck: state.user.userCheck,
+  userAddressFlag: state.user.userAddressFlag,
+})
 
 const withAuthenticaton = (Component) => {
+  const NewComponent = (props) => {
+    const dispatch = useDispatch()
+    const { userCheck, userAddressFlag } = useSelector(mapState)
+    useEffect(() => {
+      dispatch(userCheckedInSucess(true))
+      dispatch(checkUserSession())
+    }, [])
 
-    const NewComponent = (props) => {
-        const dispatch = useDispatch();
-        const { userCheck, userAddressFlag } = useSelector(mapState);
-        useEffect(() => {
-            dispatch(userCheckedInSucess(true));
-            dispatch(checkUserSession());
-            // eslint-disable-next-line react-hooks/exhaustive-deps
-        }, []);
-        // const firebase = useContext(FirebaseContext);
-        // const saveToLocalStorage = (authUser) => {
-        //     localStorage.setItem('authUser', JSON.stringify(authUser))
-        // }
-        // const next = (authUser) => {
-        //     saveToLocalStorage(authUser);
-        //     props.setAuthUser(authUser);
-        // }
-        // const fallback = () => {
-        //     localStorage.removeItem('authUser');
-        //     props.setAuthUser(null);
-        // }
-        // useEffect(() => {
-        //     const user = JSON.parse(localStorage.getItem('authUser'));
-        //     props.setAuthUser(user);
-        //     firebase.onAuthChangeListener(next, fallback);
-        // }, []);
-
-        return (
-            <div>
-                {(userCheck || userAddressFlag) &&
-                    <Loader />
-                }
-                <Component userCheck={userCheck} userAddressFlag={userAddressFlag} {...props} />
-            </div>
-
-        );
-    }
-    return NewComponent;
+    return (
+      <div>
+        {(userCheck || userAddressFlag) && <Loader />}
+        <Component
+          userCheck={userCheck}
+          userAddressFlag={userAddressFlag}
+          {...props}
+        />
+      </div>
+    )
+  }
+  return NewComponent
 }
 
-export default withAuthenticaton;
+export default withAuthenticaton
